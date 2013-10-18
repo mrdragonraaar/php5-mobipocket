06a34cbc3abff3f390f61df4d69c2037f93cedf0
mrdragonraaar <ade@mrdragonraaar.com>
2013-10-18 01:02:17 +0100

    Added lz77_encode function.

--- lz77.php
+++ lz77.php
@@ -57,4 +57,83 @@ function lz77_decode($data)
 	return $data_decoded;
 }
 
+/**
+ * Encode string to LZ77 compressed string.
+ * @param $data string.
+ * @return LZ77 compressed string.
+ */
+function lz77_encode($data)
+{
+	$data_encoded = '';
+
+	$i = 0;
+	while ($i < strlen($data))
+	{
+		$c = ord($data[$i]);
+
+		/* length, distance pair */
+		if ($i > 10 && (strlen($data) - $i) > 10)
+		{
+			$found = false;
+			for ($chunk_len = 10; $chunk_len > 2; $chunk_len--)
+			{
+				$start = substr($data, 0, $i);
+				$chunk = substr($data, $i, $chunk_len);
+				$j = strrpos($start, $chunk);
+				if ($j === false)
+					$j = $i;
+				$dist = $i - $j;
+				if ($j < $i && $dist < 2047)
+				{
+					$found = true;
+					$compound = (float)sprintf('%u',
+					   ($dist << 3) + $chunk_len - 3);
+					$data_encoded .= chr(0x80 + ($compound >> 8));
+					$data_encoded .= chr($compound & 0xFF);
+					$i += $chunk_len;
+					break;
+				}
+			}
+			if ($found)
+				continue;
+		}
+
+		$i++;
+		/* byte pair (space + char) */
+		if ($c == 32 && $i < strlen($data))
+		{
+			$n = ord($data[$i]);
+			if ($n >= 0x40 && $n <= 0x7F)
+			{
+				$data_encoded .= chr($n ^ 0x80);
+				$i++;
+				continue;
+			}
+		}
+
+		/* literal */
+		if ($c == 0 || ($c > 8 && $c < 0x80))
+			$data_encoded .= chr($c);
+		/* c literals */
+		else
+		{
+			$j = $i;
+			$data_temp = chr($c);
+			while ($j < strlen($data) && strlen($data_temp) < 8)
+			{
+				$c = ord($data[$j]);
+				if ($c == 0 || ($c > 8 && $c < 0x80))
+					break;
+				$data_temp .= chr($c);
+				$j++;
+			}
+			$i += strlen($data_temp) - 1;
+			$data_encoded .= chr(strlen($data_temp));
+			$data_encoded .= $data_temp;
+		}
+	}
+
+	return $data_encoded;
+}
+
 ?>
